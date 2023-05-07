7d822d0e31d184d164334a03e4ef2a8982bf7bd5
Boris <boris@Boriss-MacBook-Air.local>
2023-05-07 18:23:48 +0200

    Added get info button and some minor changes

--- main_gui.py
+++ main_gui.py
@@ -12,7 +12,7 @@ gui.geometry('500x720')
 gui['bg'] = '#EFEFEF'
 
 # MainText Creation - Mewloader
-main_text = tkinter.Label(gui, text="Mewloader", bg='#EFEFEF', font=('Arial', 40))
+main_text = tkinter.Label(gui, text="Mewloader", bg='#EFEFEF', font=('Arial', 40, "bold"))
 main_text.pack(fill='both', pady=20)
 
 # Label Creation
@@ -24,7 +24,7 @@ input_text = Entry(gui, width=50)
 input_text.focus_set()
 input_text.pack()
 
-def download_youtube_mp3():
+def download_audio():
     global input_text
     string = input_text.get()
     lbl.configure(text=string)
@@ -36,46 +36,61 @@ def download_youtube_mp3():
         lbl.configure(text="Not valid URL")
         return
 
-    lbl.configure(text="[-] Downloading...")
+    lbl.configure(text="[-] Downloading audio...")
     audio_file = yt.streams.filter(only_audio=True).first().download()
     base, ext = os.path.splitext(audio_file)
     new_file = base + '.mp3'
     os.rename(audio_file, new_file)
-    lbl.configure(text="[+] Download completed successfully!")
+    lbl.configure(text="[+] Audio download completed successfully!")
 
-# def download_video():
-#     global input_text
-#     string = input_text.get()
-#     lbl.configure(text=string)
+def download_video():
+    global input_text
+    string = input_text.get()
+    lbl.configure(text=string)
 
-#     url = input_text.get()
-#     try:
-#         yt = YouTube(url)
-#     except (pytube.exceptions.RegexMatchError, pytube.exceptions.VideoUnavailable):
-#         lbl.configure(text="Not valid URL")
-#         return
+    url = input_text.get()
+    try:
+        yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+
+    except (pytube.exceptions.RegexMatchError, pytube.exceptions.VideoUnavailable):
+        lbl.configure(text="Not valid URL")
+        return
     
-#     lbl.configure(text="[-] Downloading...")
-#     video_file = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
-#     video_file.download()
-#     lbl.configure(text="[+] Download completed successfully!")
-    
+    lbl.configure(text="[-] Downloading video...")
+    video_file = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
+    video_file.download()
+    lbl.configure(text="[+] Video download completed successfully!")
 
-# Button Creation
-print_button = tkinter.Button(gui, text='Download', command=download_youtube_mp3)
-print_button.pack()
+def get_link_info():
+    global input_text
+    string = input_text.get()
+    lbl.configure(text=string)
+
+    url = input_text.get()
+    try:
+        yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+    except (pytube.exceptions.RegexMatchError, pytube.exceptions.VideoUnavailable):
+        lbl.configure(text="Not valid URL")
+        return
+    
+    lbl.configure(text="Title: " + yt.title)
+
+
+    
+# Button for getting info from the given Youtube URL
+get_info = tkinter.Button(gui, text='Get Info!', command=get_link_info)
+get_info.pack()
+
+# Button for downloading audio MP3
+download_mp3 = tkinter.Button(gui, text='Download MP3', command=download_audio)
+download_mp3.pack()
+
+# Button for downloading video MP4
+download_mp4 = tkinter.Button(gui, text='Download MP4', command=download_video)
+download_mp4.pack()
 
 # Label Creation
-lbl = tkinter.Label(gui, text="")
+lbl = tkinter.Label(gui, text="", font=("Arial", 20))
 lbl.pack()
-
-
-# video_info = {
-#     "title": yt.title,
-#     "views": yt.views,
-#     "length": yt.length,
-#     "rating": yt.rating
-# }
-#
 
 gui.mainloop()
